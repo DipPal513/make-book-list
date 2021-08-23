2a0d6cc9d4ed86716e0fb77b83f0c00880faee0e
Tanvir0234 <tanvir.ali0234@gmail.com>
2021-08-23 23:49:44 +0600

    function

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
+//all input function---------
+function getItem(item){
+  const getItem = document.getElementById(item).value;
+  return getItem;
+}
+// creat Element part---------
+function inputValue(item){
+  // const bookList = document.getElementById('book-list');
+  // const newRow = document.createElement('tr');
+  const newHeading = document.createElement('th');
+   newHeading.innerHTML=getItem(item);
+  // bookList.appendChild(newHeading);
+  // bookList.appendChild(newRow);
+  return newHeading;
+}
 
+function addTotal(item){
+  const bookList = document.getElementById('book-list');
+  const newRow = document.createElement('tr');
+  const items = inputValue(item);
+  newRow.appendChild(items);
+  bookList.appendChild(newRow);
+}
 
 document.getElementById('add-book').addEventListener('click',function(){
-  const title = document.getElementById('title').value;
-  const author =document.getElementById('author').value;
-  const year = document.getElementById('year').value;
+  //const title = document.getElementById('title').value;
+ const title =getItem('title');
+  const author =getItem('author');
+  const year = getItem('year');
   const bookList = document.getElementById('book-list');
 
   if(title=='' && author==''&& year==''){
     alert('put value');
   }
   else{
-    const newRow = document.createElement('tr');
+    /*
+     const newRow = document.createElement('tr');
     //creat element
-    const newTitle = document.createElement('th');
-    newTitle.innerHTML=title;
-    newRow.appendChild(newTitle);
+    
+     const newTitle = document.createElement('th');
+     newTitle.innerHTML=title;
+     newRow.appendChild(newTitle)
+
+    
 
     
     //creat element
@@ -26,12 +53,26 @@ document.getElementById('add-book').addEventListener('click',function(){
     newYear.innerHTML=year;
     newRow.appendChild(newYear);
     bookList.appendChild(newRow);
+    */
+    //const bookList = document.getElementById('book-list');
+    inputValue('title');
+    //console.log(inputValue('title'));
+    inputValue('author');
+    //console.log( inputValue('author'))
+    inputValue('year');
+    //console.log(inputValue('year'))
+    addTotal(item)
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('year').value = '';
+
   }
 })
 
 
-/*
-    const newAuthor = document.createElement('th');
-    newAuthor.innerHTML=author;
-    newRow.appendChild(newAuthor);
-*/
+
+ /*
+     const newAuthor = document.createElement('th');
+     newAuthor.innerHTML=author;
+     newRow.appendChild(newAuthor);
+ */
